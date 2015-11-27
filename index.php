f6aaa505bf658c5547913181dc8593d561918050
mant1c0r3 <thelevimatusproject@gmail.com>
2015-11-27 01:12:36 -0400

    comment made

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <?php
-
+/* no user is logged on */
 require_once ("Includes/db.php");
 $logonSuccess = false;
 
