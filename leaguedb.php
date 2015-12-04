c167d4d7b8d8679684080c273f4e0810c828d7cb
mant1c0r3 <thelevimatusproject@gmail.com>
2015-12-03 23:58:26 -0400

    new search results page
    more thorough and helpful search results.

--- leaguedb.php
+++ leaguedb.php
@@ -12,36 +12,105 @@ and open the template in the editor.
     </head>
     <body>
         <div class="welcome">
-            Page of <?php echo htmlentities($_GET["user"])."<br/>";?>
+            Page of <?php echo htmlentities($_GET["user"])."<br/>";
+            $username = $_GET["user"];
+            ?>            
         </div>
         <?php
         require_once("Includes/db.php");
-        $userID = LeagueDB::getInstance()->get_user_id_by_username($_GET["user"]);
-        if (!$userID) {
-            exit("The user " .$_GET["user"]. " is not found. Please check the spelling and try again" );
-        }
+        //$userID = LeagueDB::getInstance()->get_user_id_by_username($_GET["user"]);
+        //if (!$userID) {
+          //  exit("The user " .$_GET["user"]. " is not found. Please check the spelling and try again" );
+        //}
         ?>
-        <div class="table">
+        <div class="ranked">
+            Ranked Stats
             <table border="black">
                 <tr>
-                    <th>username</th><th>wins</th><th>losses</th><th>kills</th><th>deaths</th><th>assists</th>
+                    <th>W/L</th><th>Kills</th><th>Deaths</th><th>Assists</th><th>CS</th>
                 </tr>  
             <?php
-            $result = LeagueDB::getInstance()->get_stats_by_user_id($userID);
-            while ($row = mysqli_fetch_array($result)) {
-                echo "<tr><td>" . htmlentities($row["username"]) . "</td>";
-                echo "<td>" . htmlentities($row["wins"]) . "</td>";
-                echo "<td>" . htmlentities($row["losses"]) . "</td>";
-                echo "<td>" . htmlentities($row["kills"]) . "</td>";
-                echo "<td>" . htmlentities($row["deaths"]) . "</td>";
-                echo "<td>" . htmlentities($row["kills"]) . "</td></tr>\n";
-            }
+            $result = LeagueDB::getInstance()->get_rankedStats_by_username($username);
+                 while($row = mysqli_fetch_array($result)) {
+                    if ($row['Win']) {
+                        echo "<tr><td>W</td>";
+                    }
+                    else {
+                      echo "<tr><td>L</td>"  ;
+                    }
+                    //echo "<tr><td>" . htmlentities($row['Win']) . "</td>";
+                    echo "<td>" . htmlentities($row['Kills']) . "</td>";
+                    echo "<td>" . htmlentities($row['Deaths']) . "</td>";
+                    echo "<td>" . htmlentities($row['Assists']) . "</td>";
+                    echo "<td>" . htmlentities($row['CS']) . "</td></tr>\n";
+                }
             mysqli_free_result($result);
             ?>
             </table>
+            <table border="black">
+                <tr><th>Kill Average</th>
+                <?php
+                $username = $_GET['user'];    
+                $result = LeagueDB::getInstance()->get_User_Ranked_Avg_Kills($username);
+                    while($row = mysqli_fetch_array($result)) {
+                        echo "<td>" . htmlentities($row['rAvgK']) . "</td></tr>\n";
+                    }
+                ?>
+                <tr><th>Death Average</th>
+                <?php
+                $username = $_GET['user'];    
+                $result = LeagueDB::getInstance()->get_User_Ranked_Avg_Deaths($username);
+                    while($row = mysqli_fetch_array($result)) {
+                        echo "<td>" . htmlentities($row['rAvgD']) . "</td></tr>\n";
+                    }
+                ?>    
+            </table>
+        </div>
+        <div class="unranked">
+            Unranked Stats
+            <table border="black">
+                <tr>
+                    <th>W/L</th><th>Kills</th><th>Deaths</th><th>Assists</th><th>CS</th>
+                </tr>  
+            <?php
+            $result = LeagueDB::getInstance()->get_normStats_by_username($username);
+                 while($row = mysqli_fetch_array($result)) {
+                    if ($row['Win']) {
+                        echo "<tr><td>W</td>";
+                    }
+                    else {
+                      echo "<tr><td>L</td>"  ;
+                    }
+                    //echo "<tr><td>" . htmlentities($row['Win']) . "</td>";
+                    echo "<td>" . htmlentities($row['Kills']) . "</td>";
+                    echo "<td>" . htmlentities($row['Deaths']) . "</td>";
+                    echo "<td>" . htmlentities($row['Assists']) . "</td>";
+                    echo "<td>" . htmlentities($row['CS']) . "</td></tr>\n";
+                }
+            mysqli_free_result($result);
+            ?>
+            </table>
+            <table border="black">
+                <tr><th>Kill Average</th>
+                <?php
+                $username = $_GET['user'];    
+                $result = LeagueDB::getInstance()->get_User_unRanked_Avg_Kills($username);
+                    while($row = mysqli_fetch_array($result)) {
+                        echo "<td>" . htmlentities($row['uAvgK']) . "</td></tr>\n";
+                    }
+                ?>
+                <tr><th>Death Average</th>
+                <?php
+                $username = $_GET['user'];    
+                $result = LeagueDB::getInstance()->get_User_unRanked_Avg_Deaths($username);
+                    while($row = mysqli_fetch_array($result)) {
+                        echo "<td>" . htmlentities($row['uAvgD']) . "</td></tr>\n";
+                    }
+                ?>    
+            </table>
         </div>
         <!-- back to main page--->
-        <div class="button">
+        <div class="dbbutton">
             <form name="backToMainPage" action="index.php">
             <input type="submit" value="Back To Main Page"/>
             </form>
